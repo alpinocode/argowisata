f7757366610d5f2716534dc2ee8ea7e745540ec3
unknown <alfinohasan629@gmail.com>
2024-11-03 00:48:35 +0700

    add to Mitra layout content

--- resources/views/argowisataContent/beranda.blade.php
+++ resources/views/argowisataContent/beranda.blade.php
@@ -1,4 +1,37 @@
 <style>
+  .slider {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 30%;
+    width: 100%;
+    overflow: hidden;
+    background: rgb(255, 255, 255);
+    border-radius: 16px;
+    box-shadow: 0 4px 30px rgb(0, 0, 0,0.1);
+    backdrop-filter: blur(5px);
+    -webkit-backdrop-filter: blur(5px);
+    border: 1px solid rgba(255, 255, 255, 0.3);
+  }
+
+  .slider-items{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    width: 25%;
+    height: 10%;
+    animation: scrolling 20s linear infinite;
+  }
+
+  @keyframes scrolling {
+    0% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(-100%);
+    }
+  }
     @media (max-width: 768px) {
         .col-md-6 {
             margin-left: 0 !important; /* Menghapus margin kiri di mobile */
@@ -12,112 +45,104 @@
 
      /* Responsive styling */
      @media (min-width: 768px) {
-              .d-flex .flex-column {
-                  margin-left: 200px;
-              }
-          }
+        .d-flex .flex-column {
+          margin-left: 200px;
+        }
+    }
       
-          @media (max-width: 767.98px) {
-              .d-flex .flex-column {
-                  margin-left: 0;
-                  text-align: center;
-              }
-          }
+    @media (max-width: 767.98px) {
+        .d-flex .flex-column {
+            margin-left: 0;
+            text-align: center;
+        }
+    }
      
 </style>
 <x-app-layout>
-    <div class="flex-wrap" >
-        {{-- image content here --}}
-        <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img class="d-block w-100" src="{{asset('image/argowisatagambar.jpg')}}" alt="First slide">
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="{{asset('image/argowisatagambar1.jpg')}}" alt="Second slide">
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="{{asset('image/argowisatagambar2.jpg')}}" alt="Third slide">
-              </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-        </div>
-
-        {{-- activity content that can be done--}}
-        <div class="d-flex flex-wrap" style="background-color: #F8F9FA; height: auto;">
-          <div class="d-flex w-100 h-100 flex-column flex-md-row" style="margin-bottom: 40px">
-              <!-- Konten Kiri -->
-              <div class="d-flex flex-column justify-content-center p-3" style="flex-basis: 50%; margin-left: 0;">
-                  <h1 class="text-start" style="font-size: 3rem; font-weight: bold;">Lokasi Baru,<br>Banyak Wahana Seru!</h1>
-                  <p class="text-start" style="font-size: 1.5rem; margin-top: 20px;">
-                      Berbagai aktifitas bisa dilakukan pengunjung, diantaranya memetik jambu, menikmati jus jambu, berkuda, memberi makan hewan ternak seperti ikan, kambing, dan kelinci, serta menanam padi di sawah. Adapun biaya yang dikeluarkan saat mengunjungi destinasi wisata ini, yakni 18 ribu berlaku untuk semua usia. Dengan tiket masuk itu, selain menikmati kebun jambu pengunjung juga akan mendapatkan jus jambu yang segar.
-                  </p>
-                  <x-primary-button class="btn btn-primary" style="background-color: #448337; border-radius: 30px; margin-top: 20px; width: 200px;">
-                      Pesan Tiket
-                  </x-primary-button>
-              </div>
-      
-              <!-- Konten Kanan -->
-              <div class="d-flex align-items-center justify-content-center" style="flex-basis: 50%;">
-                  <img src="{{asset('image/logo1.png')}}" alt="Gambar" class="img-fluid" style="max-width: 60%; height: auto; margin-top: 20px;">
-              </div>
-          </div>
+  <div class="flex-wrap" >
+    {{-- image content here --}}
+    <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="{{asset('image/argowisatagambar.jpg')}}" alt="First slide">
       </div>
-      
-         
-      
-      
-      
+      <div class="carousel-item">
+        <img class="d-block w-100" src="{{asset('image/argowisatagambar1.jpg')}}" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="{{asset('image/argowisatagambar2.jpg')}}" alt="Third slide">
+      </div>
+    </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
 
-        {{-- service Content --}}
+    {{-- activity content that can be done--}}
+    <div class="d-flex flex-wrap" style="background-color: ; height: auto;">
+      <div class="d-flex w-100 h-100 flex-column flex-md-row" style="margin-bottom: 40px">
+        <!-- Konten Kiri -->
+        <div class="d-flex flex-column justify-content-center p-3" style="flex-basis: 50%; margin-left: 0;">
+          <h1 class="text-start" style="font-size: 3rem; font-weight: bold;">Lokasi Baru,<br>Banyak Wahana Seru!</h1>
+          <p class="text-start" style="font-size: 1.5rem; margin-top: 20px;">
+            Berbagai aktifitas bisa dilakukan pengunjung, diantaranya memetik jambu, menikmati jus jambu, berkuda, memberi makan hewan ternak seperti ikan, kambing, dan kelinci, serta menanam padi di sawah. Adapun biaya yang dikeluarkan saat mengunjungi destinasi wisata ini, yakni 18 ribu berlaku untuk semua usia. Dengan tiket masuk itu, selain menikmati kebun jambu pengunjung juga akan mendapatkan jus jambu yang segar.
+          </p>
+          <x-primary-button class="btn btn-primary" style="background-color: #448337; border-radius: 30px; margin-top: 20px; width: 200px;">
+              Pesan Tiket
+          </x-primary-button>
+        </div>
         
-        <div class="container flex-wrap" style="margin-top: 100px; ">
-          <div class="row justify-content-center" >
-            
-            <!-- Card 1 -->
-            <div class="col-md-4 mb-4 d-flex justify-content-center ">
-              <div class="" style="width: 24rem; margin-top: 25px; margin-left: 25px">
-                <div class="card-body">
-                  <h3 class="card-title" style="font-size: 2.5rem; font-weight: bold">Layanan Kami</h3>
-                  <p class="card-text" style="color: #448337; font-size: 1rem; font-weight: bold">Best Nature Destination in Batam, Kepulauan Riau</p>
-                 
-                </div>
-              </div>
+          <!-- Konten Kanan -->
+        <div class="d-flex align-items-center justify-content-center" style="flex-basis: 50%;">
+          <img src="{{asset('image/logo1.png')}}" alt="Gambar" class="img-fluid" style="max-width: 60%; height: auto; margin-top: 20px;">
+        </div>
+      </div>
+    </div>
+    
+    {{-- service Content --}}
+    <div class="container flex-wrap" style="margin-top: 100px; ">
+      <div class="row justify-content-center" >
+        <!-- Card 1 -->
+        <div class="col-md-4 mb-4 d-flex justify-content-center ">
+          <div class="" style="width: 24rem; margin-top: 25px; margin-left: 25px">
+            <div class="card-body">
+              <h3 class="card-title" style="font-size: 2.5rem; font-weight: bold">Layanan Kami</h3>
+              <p class="card-text" style="color: #448337; font-size: 1rem; font-weight: bold">Best Nature Destination in Batam, Kepulauan Riau</p>
             </div>
+          </div>
+        </div>
             
-            <!-- Card 2 -->
-            <div class="col-md-4 mb-4 d-flex justify-content-center">
-              <div class="card" style="width: 24rem;">
-                <div class="card-body">
-                  <div class="d-flex align-items-center mb-2">
-                    <img src="{{ asset('image/pot.png') }}" alt="" style="width:45px; height: 45px;">
-                    <h5 class="card-title mt-2 ms-2" style="font-weight: bold; font-size: 1.5rem">Botanical Park</h5>
-                  </div>
-                  <p class="card-text" style="margin-bottom:10px">Berisi jenis-jenis tumbuhan yang ada tersedia di JMB Eco Park dan juga terdapat informasi singkat tentang tumbuhannya.</p>
-                  <hr style="border-top: 2px solid #BFBFBFdd; margin: 10px 0;">
-                  <ul>
-                    <li>Berbagai tumbuhan</li>
-                    <li>Penyejuk mata</li>
-                  </ul>
+        <!-- Card 2 -->
+        <div class="col-md-4 mb-4 d-flex justify-content-center">
+          <div class="card" style="width: 24rem;">
+            <div class="card-body">
+              <div class="d-flex align-items-center mb-2">
+                <img src="{{ asset('image/pot.png') }}" alt="" style="width:45px; height: 45px;">
+                  <h5 class="card-title mt-2 ms-2" style="font-weight: bold; font-size: 1.5rem">Botanical Park</h5>
+              </div>
+                <p class="card-text" style="margin-bottom:10px">Berisi jenis-jenis tumbuhan yang ada tersedia di JMB Eco Park dan juga terdapat informasi singkat tentang tumbuhannya.</p>
+                <hr style="border-top: 2px solid #BFBFBFdd; margin: 10px 0;">
+                <ul>
+                  <li>Berbagai tumbuhan</li>
+                  <li>Penyejuk mata</li>
+                </ul>
 
-                  <x-primary-button class="btn btn-primary d-flex align-items-center justify-content-center" style="background-color: #E9625B; margin-top: 20px; width: 100%; height: 58px;">
-                    Detail lebih lanjut
-                  </x-primary-button>
-                </div>
-              </div>
+                <x-primary-button class="btn btn-primary d-flex align-items-center justify-content-center" style="background-color: #E9625B; margin-top: 45px; width: 100%; height: 58px;">
+                  Detail lebih lanjut
+                </x-primary-button>
             </div>
+          </div>
+        </div>
           
             <!-- Card 3 -->
             
@@ -181,7 +206,7 @@
                     <li>Penyejuk mata</li>
                   </ul>
                   
-                  <x-primary-button class="btn btn-primary d-flex align-items-center justify-content-center" style="background-color: #E9625B; margin-top: 70px; width: 100%; height: 58px;">
+                  <x-primary-button class="btn btn-primary d-flex align-items-center justify-content-center " style="background-color: #E9625B; margin-top: 70px; width: 100%; height: 58px;">
                     Detail lebih lanjut
                   </x-primary-button>
                 </div>
@@ -211,7 +236,7 @@
                   </ul>
                   
 
-                  <x-primary-button class="btn btn-primary d-flex align-items-center justify-content-center" style="background-color: #E9625B; margin-top: 70px; width: 100%; height: 58px;">
+                  <x-primary-button class="btn btn-primary d-flex align-items-center justify-content-center " style="background-color: #E9625B; margin-top: 65px; width: 100%; height: 58px;">
                     Detail lebih lanjut
                   </x-primary-button>
                 </div>
@@ -239,9 +264,27 @@
                   </div>
               </div>
           </div>
+        </div>
+
+      <div>
+          <h1 style="font-weight: bold; font-size: 4rem; margin-top: 40px; text-align: center;">Mitra Kami</h1>
+          <div class="slider" style="margin-top: 40px; margin-bottom: 40px;">
+              <div class="slider-items">
+                  <!-- Item Asli -->
+                  <img src="{{asset('image/harris.png')}}" alt="Logo 1">
+                  <img src="{{asset('image/nike.jpg')}}" alt="Logo 2">
+                  <img src="{{asset('image/polo.png')}}" alt="Logo 3">
+                  <img src="{{asset('image/disnep.png')}}" alt="Logo 4">
+                  <img src="{{asset('image/coffe-shop.png')}}" alt="Logo 5">
+                  <!-- Duplikasi Item untuk Efek Infinite -->
+                  <img src="{{asset('image/harris.png')}}" alt="Logo 1">
+                  <img src="{{asset('image/nike.jpg')}}" alt="Logo 2">
+                  <img src="{{asset('image/polo.png')}}" alt="Logo 3">
+                  <img src="{{asset('image/disnep.png')}}" alt="Logo 4">
+                  <img src="{{asset('image/coffe-shop.png')}}" alt="Logo 5">
+              </div>
+          </div>
+        </div>
       </div>
-      
-      
-      
-    </div>
+  </div>
 </x-app-layout>
