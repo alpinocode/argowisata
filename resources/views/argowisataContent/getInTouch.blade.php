004a93c17a2426d23591f289b5fcbbf07dd4c04d
unknown <alfinohasan629@gmail.com>
2024-11-09 19:52:49 +0700

    add to page getIntTouch and add to responsive

--- resources/views/argowisataContent/getInTouch.blade.php
+++ resources/views/argowisataContent/getInTouch.blade.php
@@ -1,8 +1,128 @@
+<style>
+    /* Styling untuk layar dengan lebar di bawah 1024px */
+    @media(max-width: 1024px) {
+       .block-content {
+           margin-left: 40px !important; /* Mengurangi margin untuk lebih sesuai dengan tablet */
+       }
+       .img-fluid {
+           margin-left: 0 !important; /* Center image tanpa margin tambahan */
+           width: 90% !important; /* Memberi ruang yang lebih besar untuk gambar di tablet */
+       }
+       .text-content {
+           margin-top: 20px; /* Beri ruang antar konten */
+           margin-left: 0 !important; /* Tidak ada margin tambahan di kiri untuk teks */
+       }
+    }
+
+    /* Styling untuk layar dengan lebar di bawah 768px (mobile) */
+    @media(max-width: 768px) {
+       .block-content {
+           margin-left: 20px !important;
+       }
+       .img-fluid {
+           margin-left: 20px !important;
+           width: 80% !important;
+       }
+       .text-content {
+           margin-bottom: 20px !important;
+       }
+    }
+</style>
 
 <title>Get In Touch</title>
 <x-app-layout>
-    <div>
+    <div class="flex-wrap">
         <h1 class="text-center mt-4" style="font-size: 4rem;font-weight: bold;"> Get In Touch</h1>
         <p class="text-center" style="font-size: 1.5rem; color:#448337;">Layanan Agrowisata Jambu Marina Batam</p>
+        <div class="flex block-content flex-wrap" style="margin-left: 250px; margin-top: 100px">
+            <img src="{{asset('image/wisata-berkuda.jpg')}}" alt="wisata" width="40%" style="margin-top: 20px" class="img-fluid">
+            <div class="d-flex flex-column text-content" style="margin-left: 20px">
+                <h3 style="font-size: 2rem; font-weight: bold;">Botanical Park</h3>
+                <p>Horem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. 
+                    <br>Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. 
+                    <br>Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per
+                    <br> co Horem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    <br>Etiam eu turpis molestie, dictum est a, mattis tellus. 
+                    <br>Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus,
+                    <br> ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.
+                </p>
+
+                <div style="margin-top: 30px; margin-left: 40px">
+                    <div class="flex">
+                        <i class="bi bi-check-lg" style="color: #448337; font-size: 1.5rem;"></i>
+                        <p style="margin-left: 30px ">Penyejuk mata</p>
+                    </div>
+                    <div class="flex mt-2">
+                        <i class="bi bi-check-lg" style="color: #448337; font-size: 1.5rem;"></i>
+                        <p style="margin-left: 30px ">Penyejuk mata</p>
+                    </div>
+                    <div class="flex mt-2">
+                        <i class="bi bi-check-lg" style="color: #448337; font-size: 1.5rem;"></i>
+                        <p style="margin-left: 30px ">Penyejuk mata</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="flex block-content flex-wrap" style="margin-left: 250px; margin-top: 100px">
+            <img src="{{asset('image/wisata-berkuda.jpg')}}" alt="wisata" width="40%" style="margin-top: 20px" class="img-fluid">
+            <div class="d-flex flex-column " style="margin-left: 20px">
+                <h3 style="font-size: 2rem; font-weight: bold;">Botanical Park</h3>
+                <p>Horem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. 
+                    <br>Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. 
+                    <br>Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per
+                    <br> co Horem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    <br>Etiam eu turpis molestie, dictum est a, mattis tellus. 
+                    <br>Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus,
+                    <br> ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.
+                </p>
+
+                <div style="margin-top: 30px; margin-left: 40px">
+                    <div class="flex">
+                        <i class="bi bi-check-lg" style="color: #448337; font-size: 1.5rem;"></i>
+                        <p style="margin-left: 30px ">Penyejuk mata</p>
+                    </div>
+                    <div class="flex mt-2">
+                        <i class="bi bi-check-lg" style="color: #448337; font-size: 1.5rem;"></i>
+                        <p style="margin-left: 30px ">Penyejuk mata</p>
+                    </div>
+                    <div class="flex mt-2">
+                        <i class="bi bi-check-lg" style="color: #448337; font-size: 1.5rem;"></i>
+                        <p style="margin-left: 30px ">Penyejuk mata</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="flex block-content flex-wrap" style="margin-left: 250px; margin-top: 100px">
+            <img src="{{asset('image/wisata-berkuda.jpg')}}" alt="wisata" width="40%" style="margin-top: 20px" class="img-fluid">
+            <div class="d-flex flex-column " style="margin-left: 20px">
+                <h3 style="font-size: 2rem; font-weight: bold;">Botanical Park</h3>
+                <p>Horem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. 
+                    <br>Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. 
+                    <br>Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per
+                    <br> co Horem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    <br>Etiam eu turpis molestie, dictum est a, mattis tellus. 
+                    <br>Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus,
+                    <br> ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.
+                </p>
+
+                <div style="margin-top: 30px; margin-left: 40px">
+                    <div class="flex">
+                        <i class="bi bi-check-lg" style="color: #448337; font-size: 1.5rem;"></i>
+                        <p style="margin-left: 30px ">Penyejuk mata</p>
+                    </div>
+                    <div class="flex mt-2">
+                        <i class="bi bi-check-lg" style="color: #448337; font-size: 1.5rem;"></i>
+                        <p style="margin-left: 30px ">Penyejuk mata</p>
+                    </div>
+                    <div class="flex mt-2">
+                        <i class="bi bi-check-lg" style="color: #448337; font-size: 1.5rem;"></i>
+                        <p style="margin-left: 30px ">Penyejuk mata</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
 </x-app-layout>
